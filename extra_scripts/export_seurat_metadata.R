6118f294b274625fe2ef2b3bb4f3bd9e6658cab5
David A Knowles <knowles84@gmail.com>
2026-07-22 23:44:06 -0400

    Handle double-compressed GEO RDS files

--- extra_scripts/export_seurat_metadata.R
+++ extra_scripts/export_seurat_metadata.R
@@ -5,7 +5,19 @@ if (length(args) != 2) {
   stop("usage: export_seurat_metadata.R INPUT.rds[.gz] OUTPUT.tsv.gz")
 }
 
-object <- readRDS(args[[1]])
+input <- if (grepl("\\.gz$", args[[1]], ignore.case = TRUE)) {
+  outer <- gzfile(args[[1]], open = "rb")
+  magic <- readBin(outer, what = "raw", n = 2)
+  close(outer)
+  outer <- gzfile(args[[1]], open = "rb")
+  if (identical(magic, as.raw(c(0x1f, 0x8b)))) gzcon(outer) else outer
+} else {
+  args[[1]]
+}
+object <- readRDS(input)
+if (inherits(input, "connection")) {
+  close(input)
+}
 metadata <- object[[]]
 metadata <- cbind(cell_id = rownames(metadata), metadata)
 connection <- gzfile(args[[2]], open = "wt")
